13b476ab6839af9087e94de37b9a15073e37bbe1
ornstrange <orn.strange@gmail.com>
2019-11-27 22:11:16 +0000

    update

---
+++ src/curses/main.py
@@ -0,0 +1,9 @@
+import curses as cur
+
+from windows.py import Window
+from menu.py import Menu
+
+class Interface:
+    def __init__(self):
+        pass
+
---
+++ src/main.py
@@ -0,0 +1,10 @@
+from curses/main.py import Interface
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
+
