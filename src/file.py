44e7b5077d0f0ebe20298239e9e1b7737cb48fb8
jakobj17 <jakobj17@ru.is>
2019-12-02 16:25:33 +0000

    auka info

--- src/file.py
+++ src/file.py
@@ -2,10 +2,10 @@ class File:
     def writeEmployees(self, employees):
         with open("data/employees.csv", "w+") as f:
             # header row
-            f.write("name,ssn,address,landline,mobile,email\n")
+            f.write("name,ssn,address,landline,mobile,email,rank,role\n")
             # data rows
             for e in employees:
-                f.write(f"{e.name},{e.ssn},{e.address},{e.landline},{e.mobile},{e.email}\n")
+                f.write(f"{e.name},{e.ssn},{e.address},{e.landline},{e.mobile},{e.email},{e.rank},{e.role}\n")
 
 
 
--- src/objects/airplane.py
+++ src/objects/airplane.py
@@ -1,12 +1,21 @@
 class Airplane:
-    def __init__(self, id, type, maker, nrSeats):
+    def __init__(self, id, type, planeInsignia, model,nrSeats,emptyWeight, maxTakeOffWeight, unitThrust, serviceCeiling, length, height, wingspan):
         self.id = id
         self.type = type
-        self.maker = maker
+        self.planeInsignia = planeInsignia
+        self.model = model
         self.nrSeats = nrSeats
+        self.emptyWeight = emptyWeight
+        self.maxTakeOffWeight = maxTakeOffWeight
+        self.unitThrust = unitThrust
+        self.serviceCeiling = serviceCeiling
+        self.length = length
+        self.height = height
+        self.wingspan = wingspan
 
     def __str__(self):
-        return f"AIRPLANE: [{self.id}, {self.type}, {self.maker}, {self.nrSeats}]"
+        return f"AIRPLANE: [{self.id}, {self.type}, {self.planeInsignia},{self.model},{self.nrSeats},{self.emptyWeight},{self.maxTakeOffWeight},{self.unitThrust},{self.serviceCeiling},{self.length},{self.height},{self.wingspan}]"
 
     def __repr__(self):
         return self.__str__()
+
