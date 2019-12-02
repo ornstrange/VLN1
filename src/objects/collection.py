ad1c86fe47582704c7f427fd3d70988ca008f4ff
elmarolafsson <elmarolafsson@hotmail.com>
2019-12-02 13:55:08 +0000

    fix

--- src/objects/collection.py
+++ src/objects/collection.py
@@ -1,26 +1,26 @@
-from airplane.py import Airplane
-from destination.py import Destination
-from employee.py import Pilot, FlightAttendant
-from voyage.py import Voyage
+from airplane import Airplane
+from destination import Destination
+from employee import Pilot, Employee
+from voyage import Voyage
 
 
 class Airplanes:
-    def __init__(self, airplanes=None)
+    def __init__(self, airplanes=None):
         self.all = airplanes if airplanes else []
 
 
 class Voyages:
-    def __init__(self, voyages=None)
+    def __init__(self, voyages=None):
         self.all = voyages if voyages else []
 
 
 class Destinations:
-    def __init__(self, destinations=None)
+    def __init__(self, destinations=None):
         self.all = destinations if destinations else []
 
 
 class Employees:
-    def __init__(self, pilots=None, flightAttendants=None)
+    def __init__(self, pilots=None, flightAttendants=None):
         self.pilots = pilots if pilots else []
         self.flightAttendants = flightAttendants if flightAttendants else []
         self.all = self.pilots if self.pilots else []
