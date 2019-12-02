8e5099da3e42b978d56c6640bedd0c4f3ef788af
ornstrange <orn.strange@gmail.com>
2019-12-02 15:13:09 +0000

    work work work

--- src/file.py
+++ src/file.py
@@ -1,2 +1,9 @@
 class File:
-    pass
+    def writeEmployees(self, employees):
+        with open("data/employees.csv", "w+") as f:
+            # header row
+            f.write("name,ssn,address,landline,mobile,email\n")
+            # data rows
+            for e in employees:
+                f.write(f"{e.name},{e.ssn},{e.address},{e.landline},{e.mobile},{e.email}\n")
+
