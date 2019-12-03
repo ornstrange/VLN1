4f97e32d6b7429ac6a6888fa53e72868cd68e188
jakobj17 <jakobj17@ru.is>
2019-12-03 13:44:29 +0000

    öpdate a write

--- src/objects/employee.py
+++ src/objects/employee.py
@@ -1,19 +1,24 @@
 class Employee:
-    def __init__(self, name, ssn, address, landline, mobile, email, rank, license = False):
+    def __init__(self, name, ssn, address, mobile, email, rank, license = False):
         self.name = name
         self.ssn = ssn
         self.address = address
-        self.landline = landline
         self.mobile = mobile
         self.email = email
         self.rank = rank
         self.license = license
 
+
     def __str__(self):
         # csv representation
-        output = ""
-        empDict = vars(self)
-        for val in empDict.values():
-            output += f"{val},"
-        return output[:-1] # strip last comma
+        employeeDict = vars(self)
+        valuesStr = [str(x) for x in employeeDict.values()]
+        return ",".join(valuesStr)
 
+    def __repr__(self):
+        # debug repr
+        output = "Employee: ["
+        items = vars(self)
+        for key in items:
+            output += f"{key}: {items[key]}, "
+        return output.strip() + "]"
