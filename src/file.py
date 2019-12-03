596c4dafac7b84296f39740bc4f69a65b6b86cd6
jakobj17 <jakobj17@ru.is>
2019-12-03 13:59:14 +0000

    öpd file

--- src/file.py
+++ src/file.py
@@ -1,25 +1,25 @@
 class File:
-    def write(self, arr):
+    def write(self,f, arr):
         # header row
         header = vars(arr[0])
         f.write(",".join(list(header.keys())))
         # data rows
-        for e in employees:
-            f.write(str(e))
+        for object in arr:
+            f.write(str(object))
 
     def writeEmployees(self, employees):
         with open("data/employees.csv", "w+") as f:
-            self.write(employees)
+            self.write(f,employees)
 
     def writeAirplanes(self, airplanes):
         with open("data/airplaines.csv", "w+") as f:
-            self.write(airplanes)
+            self.write(f,airplanes)
 
     def writeVoyages(self, voyages):
         with open("data/voyages.csv", "w+") as f:
-            self.write(voyages)
+            self.write(f,voyages)
 
     def writeDestinations(self, destinations):
         with open("data/destinations.csv", "w+") as f:
-            self.write(destinations)
+            self.write(f,destinations)
 
