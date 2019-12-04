08ffdd762449db34b041160a9f0186b4aa7de1a9
Arnar B <arnar868@gmal.com>
2019-12-04 12:33:58 +0000

    bruh

--- src/testing.py
+++ src/testing.py
@@ -1,6 +1,16 @@
-filestream = open("./testData/Aircraft.csv","r")
 
-if filestream:
+import csv
+
+with open("Aircraft.csv", newline="") as csvfile:
+    spamreader = csv.reader(csvfile)
+
+    for row in spamreader:
+        print(",".join(row))
+
+
+#filestream = open("./testData/Aircraft.csv","r")
+
+#if filestream:
     
-    for line in filestream: 
-        print(line.strip())
+    #for line in filestream: 
+        #print(line.strip())
